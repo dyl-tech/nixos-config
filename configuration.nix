bf36ec27e4c940337c9b70dfaed379929787f5f5
dyl <kuehnldylan@gmail.com>
2025-10-09 01:17:36 -0500

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,204 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+  time.hardwareClockInLocalTime = true;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dyl = {
+    isNormalUser = true;
+    description = "dyl";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;  # <-- Make fish the default shell
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Allow fish as a login shell
+  programs.fish.enable = true;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Flatpak
+  services.flatpak.enable = true;
+
+  # Packages
+  environment.systemPackages = with pkgs; [
+    vim
+    neovim
+    fastfetch
+    kitty
+    discord
+    fish
+    steam
+    wget
+    btop
+    gparted
+    pciutils
+    git
+    gnome-extension-manager
+    prismlauncher
+    gnome-tweaks
+    libreoffice-fresh
+  ];
+
+boot.blacklistedKernelModules = [ "nouveau" ];
+
+# Enable NVIDIA + AMD hybrid graphics (Zephyrus G14)
+services.xserver.videoDrivers = [ "nvidia" ];
+
+hardware.opengl = {
+  enable = true;
+  extraPackages = with pkgs; [
+    vulkan-tools
+    vulkan-validation-layers
+    vulkan-loader
+  ];
+};
+
+hardware.nvidia = {
+  modesetting.enable = true;
+  powerManagement.enable = true;
+  nvidiaSettings = true;
+  open = false;  # keep false for proprietary drivers (better for gaming)
+  package = config.boot.kernelPackages.nvidiaPackages.latest;
+
+  prime = {
+    offload.enable = true;
+    amdgpuBusId = "PCI:101:0:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+};
+
+# Enable Steam support
+programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true;   # optional, allows streaming
+  dedicatedServer.openFirewall = true; # optional, for hosting
+};
+
+# Latest kernel is best but for now this works
+boot.kernelPackages = pkgs.linuxPackages_6_6;
+
+# Only the WMI module your laptop uses
+boot.kernelModules = [ "asus-wmi" ];
+
+# Enable Supergfxctl (GPU switching)
+services.supergfxd.enable = true;
+
+# Temporary fix for GPU detection (safe even if not needed later)
+systemd.services.supergfxd.path = [ pkgs.pciutils ];
+
+# Enable Asus control daemon + user service
+services.asusd = {
+  enable = true;
+  enableUserService = true;
+};
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
